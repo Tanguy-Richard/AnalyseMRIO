57081506d229988491efab6b012266f32e8bcc42
Tanguy-Richard <lorenzitanguy@gmail.com>
2023-02-06 18:32:55 +0100

    Ajout de version potentiellemnet mieux

---
+++ R/SPA_opt2.R
@@ -0,0 +1,64 @@
+#' SPA_opt2
+#'
+#' Construit récursivement un arbre récapitulant les effets à chaques étapes.
+#'
+#' @param f vecteur des contributions par pays_secteur par unité de production
+#' @param A matrice de Leontief
+#' @param Y Demande Finale
+#' @param Tmax Profondeur maximale autorisée
+#' @param tol Valeur limite
+#' @param Z Noeud actuel
+#' @param cc coefficient intermédiaire (cc = 1 pour Z = "Tout)
+#'
+#' @return Un data frame à 2 colonnes:
+#'  - une avec le nom du noeud
+#'  - une avec la contribution
+#' @export
+#'
+#' @importFrom data.table rbindlist
+#'
+#' @examples
+#' \dontrun{
+#'
+#' data(BASE_ECOLE)
+#'
+#' # Pour avoir la matrice de Leontief :
+#' MRIO_1990_ALL<-CompoECOLEouA17(BASE_ECOLE,"OptFullOptionsBonus",date=1990)
+#' A <- MRIO_1990_ALL[["A_tab"]][,-1] # On enlève la colonne année
+#' A <- MatCol(A,"Lig_Country","Lig_Indus")
+#'
+#' # Pour aller chercher Y (demande finale)
+#' Y <- MRIO_1990_ALL[["DF_TOT"]]
+#' Y <- Y[,-c(1,2)] # prendre uniquement la colonne d'interet
+#' Y <- MatCol(Y,"Lig_Country","Lig_Indus")
+#'
+#' # Pour aller chercher VA/PROD
+#' VA <- MRIO_1990_ALL[["VA"]]
+#' PROD <- MRIO_1990_ALL[["PROD"]]
+#' VA <- VA[,-c(1,2)] # prendre uniquement la colonne d'interet
+#' PROD <- PROD[,-c(1,2)]
+#' VA <- MatCol(VA,"Col_Country","Col_Indus")
+#' PROD <- MatCol(PROD,"Lig_Country","Lig_Indus")
+#' VA <- VA/PROD
+#'
+#'
+#' # On donnes un parcours
+#' Z <- c( "CHN_ENRJ")
+#' # On donne la valeur du coef associé au chemin
+#' cc <- Y[Z,]
+#'
+#' # Contribution du sous arbre
+#' test <- SPA_opt2(VA,A,Y,5,0.5,Z,cc)
+#' View(test)
+#'
+#' test2 <- SPA_opt2(VA,A,Y,5,0.5)
+#' View(test2)
+#'
+#' }
+SPA_opt2 <- function(f, A, Y, Tmax, tol, Z = "Tout", cc = 1){
+
+  L <- inversion_rcpp3(diag(ncol(A))-A)
+  Ftot <- Mult2_rcpp3(t(f),L)
+
+  return(rbindlist(SPA_tt3(f,A,Y,Tmax,tol,L,Ftot,Z,cc)))
+}
